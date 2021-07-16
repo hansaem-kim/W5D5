026fda815ef863869612b62eae50eefb598aab75
Hansaem Kim <hansaem_kim@hotmail.com>
2021-07-16 16:01:55 -0400

    query 3

--- skeleton/movie_buff/01_queries.rb
+++ skeleton/movie_buff/01_queries.rb
@@ -40,7 +40,7 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the
   # largest casts (i.e most actors)
-    Movie.select(:id, :title).joins(:castings).group('movies.title').order("COUNT(*) DESC").limit(3)
+    Movie.select(:id, :title).joins(:castings).group('movies.id').order("COUNT(*) DESC").limit(3)
 
 
 end
@@ -57,7 +57,7 @@ def directed_by_one_of(them)
   # Movie.where(yr: years)
   #
   # Find the id and title of all the movies directed by one of 'them'.
-  Movie.select(:id, :title).join(:director).where('name IN (?)', them)
+  Movie.select(:id, :title).joins(:director).where("name IN (?)", them)
 
 end
 
@@ -72,5 +72,5 @@ def movie_names_before_1940
   # improve performace for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
-
+  Movie.where("yr < 1940").pluck(:title)
 end
